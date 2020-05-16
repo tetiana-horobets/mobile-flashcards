6c78fba073b7823026588676359e56b382cf02b8
tetiana <tetiana.horobets@gmail.com>
2020-05-16 13:51:43 -0700

    Add Api

--- views/DeckListView.js
+++ views/DeckListView.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import Api from '../Api.js';
 
 export default class DeckListView extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.api = new Api();
+  }
+
+  componentDidMount() {
+    this.api.getDecks().then(data => {
+      console.log(data);
+    })
+  }
+
   render() {
     return <View style={styles.container}>
       <Text>Hello!</Text>
