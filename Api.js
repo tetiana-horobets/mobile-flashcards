32e53e2ad9f67d44d097bd5502ccf5ade349ef24
tetiana <tetiana.horobets@gmail.com>
2020-05-17 11:55:08 -0700

    Read data

--- Api.js
+++ Api.js
@@ -3,32 +3,7 @@ import { AsyncStorage } from 'react-native';
 export default class Api {
 
   getDecks() {
-    return Promise.resolve({
-      React: {
-        id: 'React',
-        title: 'React',
-        questions: [
-          {
-            question: 'What is React?',
-            answer: 'A library for managing user interfaces'
-          },
-          {
-            question: 'Where do you make Ajax requests in React?',
-            answer: 'The componentDidMount lifecycle event'
-          }
-        ]
-      },
-      JavaScript: {
-        id: 'JavaScript',
-        title: 'JavaScript',
-        questions: [
-          {
-            question: 'What is a closure?',
-            answer: 'The combination of a function and the lexical environment within which that function was declared.'
-          }
-        ]
-      }
-    });
+    return AsyncStorage.getItem('decks').then(data => JSON.parse(data) || {});
   }
 
   generateUID() {
