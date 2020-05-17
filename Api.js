2dce2f47eb6b184aac1b6996265b65432728f3b3
tetiana <tetiana.horobets@gmail.com>
2020-05-17 11:40:55 -0700

    Save deck

--- Api.js
+++ Api.js
@@ -1,3 +1,5 @@
+import { AsyncStorage } from 'react-native';
+
 export default class Api {
 
   getDecks() {
@@ -29,7 +31,19 @@ export default class Api {
     });
   }
 
+  generateUID() {
+    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
+  }
+
   createDeck(deck) {
-    console.log(deck);
+    const id = this.generateUID();
+    const newDeck = {};
+    newDeck[id] = {
+      id: id,
+      title: deck.title,
+      questions: deck.questions || []
+    };
+    const deckJson = JSON.stringify(newDeck);
+    AsyncStorage.mergeItem('decks', deckJson);
   }
 }
