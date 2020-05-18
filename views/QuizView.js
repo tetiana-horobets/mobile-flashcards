de2d38a02e4af548e3013a5fa15c631498df7db6
tetiana <tetiana.horobets@gmail.com>
2020-05-18 08:51:16 -0700

    Show question and answer

--- views/QuizView.js
+++ views/QuizView.js
@@ -1,8 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View, Button} from 'react-native';
 import { connect } from 'react-redux';
 
 class QuizView extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showAnswer: false
+    };
+  }
+
+  showAnswer() {
+    this.setState({showAnswer: true});
+  }
+
+  showQuestion() {
+    this.setState({showAnswer: false});
+  }
 
   render() {
     const deck = this.props.decks[this.props.selectedDeck];
@@ -13,8 +29,24 @@ class QuizView extends React.Component {
       </View>;
     }
 
+    const question = deck.questions[0];
+
+    if (this.state.showAnswer) {
+      return <View style={styles.container}>
+        <Text>{question.answer}</Text>
+        <Button
+          title="Show question"
+          onPress={() => this.showQuestion()}
+        />
+      </View>;
+    }
+
     return <View style={styles.container}>
-      <Text>QuizView</Text>
+      <Text>{question.question}</Text>
+      <Button
+        title="Show answer"
+        onPress={() => this.showAnswer()}
+      />
     </View>;
   }
 }
