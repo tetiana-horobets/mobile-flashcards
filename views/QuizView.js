8b9dd0f9441fc3b4472917013947368892c14ba3
tetiana <tetiana.horobets@gmail.com>
2020-05-18 08:40:37 -0700

    Add worning if there are no questions

--- views/QuizView.js
+++ views/QuizView.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View} from 'react-native';
+import { connect } from 'react-redux';
 
-export default class QuizView extends React.Component {
+class QuizView extends React.Component {
 
   render() {
+    const deck = this.props.decks[this.props.selectedDeck];
+
+    if (!deck.questions.length) {
+      return <View style={styles.container}>
+        <Text>Sorry, you cannot take a quiz because there are no cards in the deck.</Text>
+      </View>;
+    }
+
     return <View style={styles.container}>
       <Text>QuizView</Text>
     </View>;
@@ -18,3 +27,17 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const mapStateToProps = state => {
+  return {
+    decks: state.decks,
+    selectedDeck: state.selectedDeck
+  };
+}
+
+const mapDispatchToProps = {}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(QuizView);
